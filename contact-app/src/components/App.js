9b1a06ffda9f6594119e1bbde39c08b163af9d70
abhishek siddappagari <abhishek.siddappagari456@gmail.com>
2023-05-06 13:20:48 +0530

    initialzing

---
+++ contact-app/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Headr from  "./Headr";
+import AddContat from  "./AddContat";
+import ContactList from  "./ContactList";
+
+
+function App() {
+  return (
+      <div>
+        <Headr/>
+        {/* <AddContat/>
+        <ContactList/> */}
+      </div>
+  );
+}
+
+export default App;
---
+++ contact-app/src/components/Headr.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Headr = () =>{
+    return(
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Contact Manager</h2>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Headr
